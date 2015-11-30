fcb2b2206947da94ad13f81e80668fe5539859b7
stanislas-brossette <stanislas.brossette@gmail.com>
2015-11-30 16:48:02 +0900

    Add inverse static algo

---
+++ src/IS.cpp
@@ -0,0 +1,114 @@
+// This file is part of RBDyn.
+//
+// RBDyn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RBDyn is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with RBDyn.  If not, see <http://www.gnu.org/licenses/>.
+#include <iostream>
+
+// associated header
+#include "IS.h"
+
+// includes
+// RBDyn
+#include "MultiBody.h"
+#include "MultiBodyConfig.h"
+
+namespace rbd
+{
+
+InverseStatics::InverseStatics(const MultiBody& mb):
+	f_(mb.nrBodies())
+{
+}
+
+void InverseStatics::inverseStatics(const MultiBody& mb, MultiBodyConfig& mbc)
+{
+	const std::vector<Body>& bodies = mb.bodies();
+	const std::vector<Joint>& joints = mb.joints();
+	const std::vector<int>& pred = mb.predecessors();
+
+	sva::MotionVecd a_0(Eigen::Vector3d::Zero(), mbc.gravity);
+
+	for(std::size_t i = 0; i < bodies.size(); ++i)
+	{
+		const sva::PTransformd& X_p_i = mbc.parentToSon[i];
+
+		if(pred[i] != -1)
+			mbc.bodyAccB[i] = X_p_i*mbc.bodyAccB[pred[i]];
+		else
+			mbc.bodyAccB[i] = X_p_i*a_0;
+
+		f_[i] = bodies[i].inertia()*mbc.bodyAccB[i] -
+			mbc.bodyPosW[i].dualMul(mbc.force[i]);
+	}
+
+
+	for(int i = static_cast<int>(bodies.size()) - 1; i >= 0; --i)
+	{
+		for(int j = 0; j < joints[i].dof(); ++j)
+		{
+			mbc.jointTorque[i][j] = mbc.motionSubspace[i].col(j).transpose()*
+				f_[i].vector();
+		}
+
+		if(pred[i] != -1)
+		{
+			const sva::PTransformd& X_p_i = mbc.parentToSon[i];
+			f_[pred[i]] = f_[pred[i]] + X_p_i.transMul(f_[i]);
+		}
+	}
+
+  std::cout << "mb.bodies().size() = \n" << mb.bodies().size() << std::endl;
+  std::cout << "mb.joints().size() = \n" << mb.joints().size() << std::endl;
+  std::cout << "mb.predecessors().size() = \n" << mb.predecessors().size() << std::endl;
+
+  for (size_t i = 0; i < mbc.q.size(); ++i)
+    for (size_t j = 0; j < mbc.q[i].size(); ++j)
+      std::cout << "mbc.q[" << i << "][" << j << "]: " << mbc.q[i][j] << std::endl;
+
+  std::cout << "a_0 = \n" << a_0 << std::endl;
+  for (size_t i = 0; i < mbc.force.size(); ++i)
+    std::cout << "force[" << i << "]: " << mbc.force[i] << std::endl;
+  for (size_t i = 0; i < f_.size(); ++i)
+    std::cout << "f_[" << i << "]: " << f_[i] << std::endl;
+  for (size_t i = 0; i < mbc.jointTorque.size(); ++i)
+    for (size_t j = 0; j < mbc.jointTorque[i].size(); ++j)
+      std::cout << "torque[" << i << "][" << j << "]:" << mbc.jointTorque[i][j] << std::endl;
+
+}
+
+
+
+void InverseStatics::sInverseStatics(const MultiBody& mb, MultiBodyConfig& mbc)
+{
+	checkMatchAlphaD(mb, mbc);
+	checkMatchForce(mb, mbc);
+	checkMatchJointConf(mb, mbc);
+	checkMatchJointVelocity(mb, mbc);
+	checkMatchBodyPos(mb, mbc);
+	checkMatchParentToSon(mb, mbc);
+	checkMatchBodyVel(mb, mbc);
+	checkMatchMotionSubspace(mb, mbc);
+
+	checkMatchBodyAcc(mb, mbc);
+	checkMatchJointTorque(mb, mbc);
+
+  inverseStatics(mb, mbc);
+}
+
+
+const std::vector<sva::ForceVecd>& InverseStatics::f() const
+{
+	return f_;
+}
+
+} // namespace rbd
---
+++ src/IS.h
@@ -0,0 +1,71 @@
+// This file is part of RBDyn.
+//
+// RBDyn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RBDyn is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with RBDyn.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// includes
+// std
+#include <vector>
+
+// SpaceVecAlg
+#include <SpaceVecAlg/SpaceVecAlg>
+
+namespace rbd
+{
+class MultiBody;
+struct MultiBodyConfig;
+
+/**
+	* Inverse Dynamics algorithm.
+	*/
+class InverseStatics
+{
+public:
+	InverseStatics()
+	{}
+	/// @param mb MultiBody associated with this algorithm.
+	InverseStatics(const MultiBody& mb);
+
+	/**
+		* Compute the inverse statics.
+		* @param mb MultiBody used has model.
+		* @param mbc Uses force, parentToSon, bodyPosW, parentToSon, motionSubspace
+    * and gravity.
+		* Fill bodyAccB and jointTorque.
+		*/
+	void inverseStatics(const MultiBody& mb, MultiBodyConfig& mbc);
+
+	// safe version for python binding
+
+	/** safe version of @see inverseDynamics.
+		* @throw std::domain_error If mb don't match mbc.
+		*/
+	void sInverseStatics(const MultiBody& mb, MultiBodyConfig& mbc);
+
+	/**
+		* @brief Get the internal forces.
+		* @return vector of forces transmitted from body λ(i) to body i across
+		* joint i.
+		*/
+	const std::vector<sva::ForceVecd>& f() const;
+
+private:
+	/// @brief Internal forces.
+	/// f_ is the vector of forces transmitted from body λ(i) to body i across
+	/// joint i.
+	std::vector<sva::ForceVecd> f_;
+};
+
+} // namespace rbd
